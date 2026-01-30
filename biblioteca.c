44e27e29c4438a5daaac69f79a0353d2a6c64b66
jairomachado07 <jr.hk22@gmail.com>
2026-01-30 20:39:10 -0300

    adicionando práticas de alocação, ponteiros e outros recursos.

--- biblioteca.c
+++ biblioteca.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+// Definições de constantes
 #define MAX_LIVROS 50
 #define TAM_STRING 100
+
+// Definição da struct Livro
+struct Livro {
+    char nome[TAM_STRING];
+    char autor[TAM_STRING];
+    char editora[TAM_STRING];
+    int edicao;
+};
+
+//Função para limpar o buffer do teclado
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
